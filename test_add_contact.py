a1c260d73ca5e4f21c79fc570a39a21216e974fd
Olya Chechenina <checheninao@gmail.com>
2016-11-10 21:58:59 +0300

    задание4: выделена фикстура и осуществлен переход на py.test

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,33 +1,26 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
 from phones import Phones
 from emails import Emails
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        allphones = Phones(homephone="8(017)2851111", mobilephone="+375297111111", workphone="8(017)2841111",
-                           faxphone="8(017)2841111")
-        allemails = Emails(email1 = "111@mail.ru", email2 = "222@gmail.com", email3="333@gmail.com")
-        self.app.create_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov",
+
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    allphones = Phones(homephone="8(017)2851111", mobilephone="+375297111111", workphone="8(017)2841111",
+                    faxphone="8(017)2841111")
+    allemails = Emails(email1 = "111@mail.ru", email2 = "222@gmail.com", email3="333@gmail.com")
+    app.create_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov",
                                                nickname="Iva", title="zaq", company="comp1", address="st. Qwerty 1",
                                                phones = allphones, emails = allemails))
-        self.app.logout()
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == '__main__':
-    unittest.main()
--- test_add_group.py
+++ test_add_group.py
@@ -1,31 +1,25 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from application import Application
 from group import Group
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="group1", header="person1", footer="person2"))
-        self.app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group1", header="person1", footer="person2"))
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
+
