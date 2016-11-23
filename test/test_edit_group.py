0a041cd6f1cf5d899bc7b448ed07f2939cbc2111
Olya Chechenina <checheninao@gmail.com>
2016-11-23 22:48:13 +0300

    добавлены проверки длин списков групп до и после выполнения добавления, удаления и редактирования групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,16 +5,25 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_group1"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="group2", header="person3", footer="person4"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_group2"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="group3"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_group3"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="person5"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
