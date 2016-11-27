58ec076285851d03166fd65c8f596a007ed71ddf
Olya Chechenina <checheninao@gmail.com>
2016-11-27 22:16:28 +0300

    реализована параметризация тестов для создания групп и контактов

--- fixture/group.py
+++ fixture/group.py
@@ -87,7 +87,8 @@ class GroupHelper:
             self.open_groups_page()
             self.group_cache = []
             for element in wd.find_elements_by_css_selector("span.group"):
-                text = element.text
+                #text = element.text  #отсекает пробелы в конце названия группы
+                text2 = element.find_element_by_name("selected[]").get_attribute("title")[8:-1] # title="Select (text2)"
                 id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.group_cache.append(Group(name=text, id=id))
+                self.group_cache.append(Group(name=text2, id=id))
         return list(self.group_cache)
