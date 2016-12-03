482047884b90859caba2499c8d0f54e7ac414d88
Olya Chechenina <checheninao@gmail.com>
2016-12-03 17:15:20 +0300

    для сохранения структуры объектов в формат JSON использована библиотека jsonpickle

--- generator/contact.py
+++ generator/contact.py
@@ -4,7 +4,7 @@ from model.phones import Phones
 import os.path
 import string
 import random
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -53,4 +53,5 @@ testdata = [Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov",
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
